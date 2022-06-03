57195ccf52a7f73de911503aae230b7cb7f74c7b
Englader <markoski.stefan@yahoo.com>
2022-06-03 14:40:16 +0200

    fixed typos

--- scripts/download_test_data.py
+++ scripts/download_test_data.py
@@ -5,13 +5,13 @@ import boto3
 # secret = ''
 # token = ''
 # s3 = boto3.client('s3', aws_access_key_id=access, aws_secret_access_key=secret, aws_session_token=token)
-# s3.download_file('alphafold-dbs-mirrors', 'sorted_opt', 'sorted_opt')
+# s3.download_file('alphafold-dbs-mirrors', 'bfd_metaclust_clu_complete_id30_c90_final_seq', 'bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt')
 # s3.download_file('alphafold-dbs-mirrors', 'mgy_clusters_2018_12.fa', 'mgy_clusters_2019_12.fa')
-# s3.download_file('alphafold-dbs-mirrors', 'pdb70', 'pdb70')
+# s3.download_file('alphafold-dbs-mirrors', 'pdb70', 'pdb70.fa')
 # s3.download_file('alphafold-dbs-mirrors', 'obsolete.dat', 'obsolete.dat')
 # s3.download_file('alphafold-dbs-mirrors', 'mmcif_files', 'mcif_files')
 # s3.download_file('alphafold-dbs-mirrors', 'pdb_seqres.txt', 'pdb_seqres.txt')
-# s3.download_file('alphafold-dbs-mirrors', 'bfd-first_non_consensus_sequences.fasta', 'bfd-first_non_concensus_sequences.fasta')
+# s3.download_file('alphafold-dbs-mirrors', 'bfd-first_non_consensus_sequences.fasta', 'bfd-first_non_consensus_sequences.fasta')
 # s3.download_file('alphafold-dbs-mirrors', 'uniclust30_2018_08', 'uniclust30_2018_08')
 # s3.download_file('alphafold-dbs-mirrors', 'uniprot.fasta', 'uniprot.fasta')
 # s3.download_file('alphafold-dbs-mirrors', 'uniref90.fasta', 'uniref90.fasta')
@@ -19,7 +19,7 @@ import boto3
 
 # comment this for local testing
 s3 = boto3.client('s3')
-s3.download_file('alphafold-dbs-mirrors', 'sorted_opt', '/mnt/bfd_database_path/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt')
+s3.download_file('alphafold-dbs-mirrors', 'bfd_metaclust_clu_complete_id30_c90_final_seq', '/mnt/bfd_database_path/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt')
 s3.download_file('alphafold-dbs-mirrors', 'mgy_clusters_2018_12.fa', '/mnt/mgnify_database_path/mgy_clusters_2019_12.fa')
 s3.download_file('alphafold-dbs-mirrors', 'pdb70', '/mnt/pdb70_database_path/pdb70')
 s3.download_file('alphafold-dbs-mirrors', 'obsolete.dat', '/mnt/obsolete_pdbs_path/obsolete.dat')
